94bebbba3fcd4601d9891d93217dd926f1a262e5
CHOICORE <choijaehyeong93@gmail.com>
2024-10-20 19:38:18 +0900

    test: 과금 전략 결정 시나리오 테스트

---
+++ src/test/kotlin/me/choicore/samples/charge/context/ScenarioTests.kt
@@ -0,0 +1,55 @@
+package me.choicore.samples.charge.context
+
+import me.choicore.samples.charge.context.ChargingMode.SURCHARGE
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.DayOfWeek.SUNDAY
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.TemporalAdjuster
+import java.time.temporal.TemporalAdjusters
+
+class ScenarioTests {
+    @Test
+    fun t1() {
+        val surchargeTimeline: Timeline =
+            Timeline(SUNDAY).apply {
+                this.addSlot(LocalTime.of(10, 0), LocalTime.of(12, 0))
+                this.addSlot(LocalTime.of(14, 0), LocalTime.of(16, 0))
+            }
+
+        val surchargeStrategy = ChargingStrategy(mode = SURCHARGE, rate = 20, timeline = surchargeTimeline)
+
+        val dayOfWeekChargingStrategyRegistry: DayOfWeekChargingStrategyRegistry =
+            DayOfWeekChargingStrategyRegistry().apply {
+                this.register(surchargeStrategy)
+            }
+
+        val discountTimeline: Timeline =
+            Timeline(LocalDate.now().with(TemporalAdjusters.previousOrSame(SUNDAY))).apply {
+                this.addSlot(LocalTime.of(10, 0), LocalTime.of(12, 0))
+                this.addSlot(LocalTime.of(14, 0), LocalTime.of(16, 0))
+            }
+
+        val discountStrategy = ChargingStrategy(mode = ChargingMode.DISCHARGE, rate = 10, timeline = discountTimeline)
+
+        val specifiedDateChargingStrategyRegistry: SpecifiedDateChargingStrategyRegistry =
+            SpecifiedDateChargingStrategyRegistry().apply {
+                this.register(discountStrategy)
+            }
+
+        val registries: List<AbstractChargingStrategyRegistry<out TemporalAdjuster>> =
+            listOf(
+                specifiedDateChargingStrategyRegistry,
+                dayOfWeekChargingStrategyRegistry,
+            )
+
+        for (registry: ChargingStrategyRegistry in registries) {
+            val strategies: List<ChargingStrategy> = registry.getChargingStrategies(LocalDate.now().plusWeeks(1))
+            if (strategies.isNotEmpty()) {
+                Assertions.assertThat(strategies.first()).isEqualTo(surchargeStrategy)
+                return
+            }
+        }
+    }
+}
