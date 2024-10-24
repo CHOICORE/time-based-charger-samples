faf34caf5176ccb1051d337ed3bd2dfa987722be
CHOICORE <choijaehyeong93@gmail.com>
2024-10-24 19:04:17 +0900

    feat: 과금 모델 audit 기능 추가

--- src/main/kotlin/me/choicore/samples/charge/context/ChargingStrategy.kt
+++ src/main/kotlin/me/choicore/samples/charge/context/ChargingStrategy.kt
@@ -15,6 +15,23 @@ data class ChargingStrategy(
 
     fun supports(date: LocalDate): Boolean = this.timeline.satisfiedBy(date = date)
 
+    fun audit(charge: Charge) {
+        require(this.supports(charge.date)) { "The specified date does not satisfy the timeline." }
+
+        this.timeline.slots.forEach { slot ->
+            val basis: TimeSlot = slot
+            val applied: TimeSlot? = slot.extractWithin(charge.start, charge.end)
+            if (applied != null) {
+                charge.adjust(
+                    mode = this.mode,
+                    rate = this.rate,
+                    basis = basis,
+                    applied = applied,
+                )
+            }
+        }
+    }
+
     companion object {
         fun noop(dayOfWeek: DayOfWeek): ChargingStrategy =
             ChargingStrategy(
--- src/test/kotlin/me/choicore/samples/charge/context/ChargingStrategyTests.kt
+++ src/test/kotlin/me/choicore/samples/charge/context/ChargingStrategyTests.kt
@@ -1,33 +1,67 @@
 package me.choicore.samples.charge.context
 
+import me.choicore.samples.charge.context.ChargingMode.DISCHARGE
+import me.choicore.samples.charge.context.ChargingMode.SURCHARGE
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalTime
 import java.time.temporal.TemporalAdjusters
 
 class ChargingStrategyTests {
     @Test
     fun `should return true when strategy is supported for specified date`() {
         val date: LocalDate = LocalDate.now()
-        val chargingStrategy = ChargingStrategy(ChargingMode.SURCHARGE, 10, Timeline.fullTime(date))
-        assertThat(chargingStrategy.supports(date)).isTrue
+        val chargingStrategy = ChargingStrategy(mode = SURCHARGE, rate = 10, timeline = Timeline.fullTime(date))
+        assertThat(chargingStrategy.supports(date = date)).isTrue
     }
 
     @Test
     fun `should return false when strategy is not supported for specified date`() {
         val date: LocalDate = LocalDate.now()
-        val chargingStrategy = ChargingStrategy(ChargingMode.SURCHARGE, 10, Timeline.fullTime(date))
-        assertThat(chargingStrategy.supports(date.plusDays(1))).isFalse
+        val chargingStrategy = ChargingStrategy(mode = SURCHARGE, rate = 10, timeline = Timeline.fullTime(date))
+        assertThat(chargingStrategy.supports(date = date.plusDays(1))).isFalse
     }
 
     @ParameterizedTest
     @EnumSource(value = DayOfWeek::class)
     fun `should return true when strategy is supported for specified day of week`(dayOfWeek: DayOfWeek) {
         val date: LocalDate = LocalDate.now().with(TemporalAdjusters.previousOrSame(dayOfWeek))
-        val chargingStrategy = ChargingStrategy(ChargingMode.SURCHARGE, 10, Timeline.fullTime(date))
-        assertThat(chargingStrategy.supports(date)).isTrue
+        val chargingStrategy =
+            ChargingStrategy(
+                mode = SURCHARGE,
+                rate = 10,
+                timeline = Timeline.fullTime(date = date),
+            )
+        assertThat(chargingStrategy.supports(date = date)).isTrue
+    }
+
+    @Test
+    fun `should audit and apply charge adjustment with correct amount`() {
+        // given
+        val charge =
+            Charge(
+                date = LocalDate.now(),
+                start = LocalTime.MIN,
+                end = LocalTime.MAX,
+            )
+
+        val chargingStrategy =
+            ChargingStrategy(
+                mode = DISCHARGE,
+                rate = 10,
+                timeline = Timeline.fullTime(LocalDate.now()),
+            )
+
+        // when
+        chargingStrategy.audit(charge = charge)
+
+        // then
+        assertThat(charge.adjustments).hasSize(1)
+        assertThat(charge.adjustments[0].amount).isEqualTo(1296L)
+        println(charge.amount)
     }
 }
